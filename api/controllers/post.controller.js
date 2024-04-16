7ddb0228e383e2060474404aa22e2fdb9964fd29
TihamShafi <tiham56@gmail.com>
2024-04-17 02:49:33 +0600

    Bookmark done

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 import prisma from "../lib/prisma.js";
 
 export const getPosts = async (req,res) => {
@@ -38,7 +39,31 @@ export const getPost = async (req,res) => {
         },
       },
     });
-    res.status(200).json(post);
+
+    let userId;
+
+    const token = req.cookies?.token;
+    if(!token) {
+      userId = null;
+    }else{
+      jwt.verify(token, process.env.JWT_SECRET_KEY, async (err,payload) => {
+
+        if(err){
+          userId = null;
+        }else{
+          userId=payload.id;
+        }
+      })
+    }
+    const saved = await prisma.savedPost.findUnique({
+      where: {
+        userId_postId: {
+          userId,
+          postId:id,
+        },
+      },
+    })
+    res.status(200).json({...post,isSaved: saved ? true : false});
   }catch(err){
     console.log(err);
     res.status(500).json({message: "Failed to get post!"});
